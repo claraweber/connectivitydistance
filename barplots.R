1462d8f8c7ec82a19314f0d4b862ee14bc5caf47
Clara Weber <88279664+claraweber@users.noreply.github.com>
2022-10-01 19:11:44 +0200

    Update barplots.R

--- barplots.R
+++ barplots.R
@@ -1,15 +1,3 @@
-# activate when needed - packages
-#install.packages('ggplot2')
-#install.packages('dplyr')
-#install.packages('tidyverse')
-#install.packages('reshape2')
-#install.packages('ggbreak')
-#install.packages('fmsb')
-#install.packages('data.table')
-#install.packages("devtools")
-#install.packages("rmatio")
-
-
 # environment
 library(ggplot2)
 library(rmatio)
@@ -21,11 +9,12 @@ library(fmsb)
 library(data.table)
 library(devtools)
 
-wd = '/Users/claraweber/Desktop/mica_distfc'
+wd = '/'
 setwd(wd)
 
 # read data
-data = read.csv('meancdfc_yeo.csv') # input here is csv file with mean values of functional connectivity and connectivity distance in each yeo network (see python function)
+data = read.csv('meancdfc_yeo.csv') 
+# input here is csv file with mean values of functional connectivity and connectivity distance in each yeo network (see python function)
 
 # subset dataframes
 cdplot <- data.frame(group=rep(c("ASD",
@@ -35,15 +24,6 @@ cdplot <- data.frame(group=rep(c("ASD",
                     y=c(data$CD_ASD, data$CD_TDC
                         #data$CD_mean
                         ))
-
-fcplot <- data.frame(group=rep(c("ASD",
-                                 "Control" ), 
-                               each=7),
-                     x=rep(c(data$X),2),
-                     y=c(data$FC_ASD, data$FC_TDC
-                         #data$FC_mean
-                         ))
-
 
 # bar plot
 cd <- ggplot(cdplot, aes(x=x, y=y, fill=group)) + 
@@ -61,34 +41,13 @@ cd <- ggplot(cdplot, aes(x=x, y=y, fill=group)) +
                             "Yeo6" = "SMN",
                             "Yeo7" = "VAN"))
 
-fc <- ggplot(fcplot, aes(x=x, y=y, fill=group)) + 
-  geom_bar(stat="identity", width=0.5,position=position_dodge()) +
-  coord_cartesian(ylim=c(0.7,0.9)) + 
-  theme_light() +
-  xlab('Yeo Networks') + 
-  ylab('Mean Functional Connectivity') +
-  labs(title='Functional Connectivity') +
-  scale_x_discrete(labels=c("Yeo1" = "DAN",
-                            "Yeo2" = "FPN",
-                            "Yeo3" = "DMN",
-                            "Yeo4" = "Visual",
-                            "Yeo5" = "Limbic",
-                            "Yeo6" = "SMN",
-                            "Yeo7" = "VAN"))
-# show ggplots next to each other
-cd | fc
-
 # inter network barplots
 cdplotinter <- data.frame(group=rep(c("ASD",
                                  "Control"), each=7),
                      x=rep(c(data$X),2),
                      y=c(data$CD_ASDinter, data$CD_TDCinter))
 
-fcplotinter <- data.frame(group=rep(c("ASD",
-                                 "Control"), each=7),
-                          x=rep(c(data$X),2),
-                          y=c(data$FC_ASDinter, data$FC_TDCinter))
-                          
+
 cdplotinter <- ggplot(cdplotinter, aes(x=x, y=y, fill=group)) + 
   geom_bar(stat="identity", width=0.5,position=position_dodge()) +
   coord_cartesian(ylim=c(50,70)) + 
@@ -103,19 +62,3 @@ cdplotinter <- ggplot(cdplotinter, aes(x=x, y=y, fill=group)) +
                             "Yeo5" = "Limbic",
                             "Yeo6" = "SMN",
                             "Yeo7" = "VAN"))
-
-fcplotinter <- ggplot(fcplotinter, aes(x=x, y=y, fill=group)) + 
-  geom_bar(stat="identity", width=0.5,position=position_dodge()) +
-  coord_cartesian(ylim=c(4,5)) + 
-  theme_light() +
-  xlab('Yeo Networks') + 
-  ylab('Mean Functional Connectivity') +
-  labs(title='Inter-Network Functional Connectivity') +
-  scale_x_discrete(labels=c("Yeo1" = "DAN",
-                            "Yeo2" = "FPN",
-                            "Yeo3" = "DMN",
-                            "Yeo4" = "Visual",
-                            "Yeo5" = "Limbic",
-                            "Yeo6" = "SMN",
-                            "Yeo7" = "VAN"))
-fcplotinter | cdplotinter
