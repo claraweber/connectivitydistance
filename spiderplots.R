ad75645b2292a20fa2968445551dd02c567f200e
Clara Weber <88279664+claraweber@users.noreply.github.com>
2022-10-01 19:16:10 +0200

    Update spiderplots.R

--- spiderplots.R
+++ spiderplots.R
@@ -1,15 +1,3 @@
-# activate when needed - packages
-#install.packages('ggplot2')
-#install.packages('dplyr')
-#install.packages('tidyverse')
-#install.packages('reshape2')
-#install.packages('ggbreak')
-#install.packages('fmsb')
-#install.packages('data.table')
-#install.packages("devtools")
-#install.packages("rmatio")
-
-
 # environment
 library(ggplot2)
 library(rmatio)
@@ -21,21 +9,20 @@ library(fmsb)
 library(data.table)
 library(devtools)
 
-wd = '/Users/claraweber/Desktop/mica_distfc'
+wd = '/'
 setwd(wd)
 
 # read data
-data = read.csv('meancdfc_yeo.csv') # input here is csv file with mean values of functional connectivity and connectivity distance in each yeo network (see python function)
+data = read.csv('meancdfc_yeo.csv') 
+# input here is csv file with mean values of functional connectivity and connectivity distance in each yeo network (see python function)
 
 
 # spider plot
 set.seed(1342)
 cd_df <-rbind(data$CD_ASD, data$CD_TDC)
-#cd_spidf <- as.data.frame(t(as.matrix(cd_df)))
 colnames(cd_df) <- c("DAN", "FPN", "DMN", "Visual", "Limbic","SMN", "VAN")
 rownames(cd_df) <- c('ASD', 'Control')
-cd_df <- rbind(rep(9) , rep(12) , cd_df)
-df <- as.data.frame(cd_df)
+df <- as.data.frame(rbind(rep(9) , rep(12) , cd_df))
 
 colors_border <-c(rgb(80/255,199/255,199/255), rgb(252/255, 119/255, 119/255)) # colors similar to ggplot default colors used for ASD/Control in bar plots
 radarchart(df, axistype=1 , 
